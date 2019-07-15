de9b95cbebac757c6565816da49fd4162593f148
AnaLiviaBR <analivia.jornalismo@gmail.com>
2019-07-15 09:03:36 -0300

    Questões 1 e 2 prontas

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tarefa PHP - LOOOOPS!</title>
+</head>
+<body>
+      <ol>
+          <li>
+              <p>Utilizando um for, imprimir os números de 1 a 100.</p>
+              <div>
+                  <?php>
+                    for <$i =1; $1 <= mt_rand(0,100); $i++){
+                         echo ($i. ',');
+                    }
+                  ?>
+            </div>
+           </li>
+        </ol>   
+</body>
+</html>
